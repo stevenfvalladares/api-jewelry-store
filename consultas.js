7f6bd2e11edd7d09bdcdadca679f999454e72924
Steven Valladares <stevenfvalladares@gmail.com>
2023-02-05 23:11:12 -0300

    The get route is created with the requirements requested by the client

--- consultas.js
+++ consultas.js
@@ -1,6 +1,7 @@
 const { Pool } = require("pg");
+const format = require("pg-format");
 
-require("dotenv").config({ path: "./.env" })
+require("dotenv").config({ path: "./.env" });
 
 const pool = new Pool({
   host: process.env.HOST,
@@ -11,4 +12,32 @@ const pool = new Pool({
   allowExitOnIdle: true,
 });
 
-module.exports = {};
+const getJewelry = async ({
+  limits = "all",
+  order_by = "id_ASC",
+  page = 1,
+}) => {
+  if (page <= 0) {
+    throw new Error("Page number cannot be equal to 0");
+  }
+
+  const offset = (page - 1) * limits;
+  const [field, address] = order_by.split("_");
+
+  if (!field || !address) {
+    throw new Error("ORDER BY incorrectly declared!");
+  }
+
+  const formattedQuery = format(
+    "SELECT * FROM inventario ORDER BY %s %s LIMIT %s OFFSET %s",
+    field,
+    address,
+    limits,
+    offset
+  );
+
+  const { rows: joyas } = await pool.query(formattedQuery);
+  return joyas;
+};
+
+module.exports = { getJewelry };
--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const express = require("express");
 const app = express();
 
 app.listen(3000, console.log("Server running from port 3000"));
+
+const { getJewelry } = require("./consultas");
+
+app.get("/joyas", async (req, res) => {
+  try {
+    const queryStrings = req.query;
+    const joyas = await getJewelry(queryStrings);
+    res.json(joyas);
+  } catch (err) {
+    console.error(err);
+    res.send(err.message);
+  }
+});
