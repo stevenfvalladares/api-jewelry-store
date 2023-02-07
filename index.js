c07d80608840f6d631b2180ed5858de1387b2b78
Steven Valladares <stevenfvalladares@gmail.com>
2023-02-06 22:09:59 -0300

    Added the function to return the HATEOAS structure in the main server file index.js

--- index.js
+++ index.js
@@ -5,11 +5,34 @@ app.listen(3000, console.log("Server running from port 3000"));
 
 const { getJewelry, getJewelryByFilters } = require("./consultas");
 
+const prepareHATEOAS = (items) => {
+  const results = items
+    .map((item) => {
+      return {
+        name: item.nombre,
+        href: `/joyas/joya/${item.id}`,
+      };
+    })
+    .slice(0, 4);
+  const totalJoyas = items.length;
+  let stockTotal = items.reduce(
+    (accumulator, current) => accumulator + current.stock,
+    0
+  );
+  const HATEOAS = {
+    totalJoyas,
+    stockTotal,
+    results,
+  };
+  return HATEOAS;
+};
+
 app.get("/joyas", async (req, res) => {
   try {
     const queryStrings = req.query;
     const joyas = await getJewelry(queryStrings);
-    res.json(joyas);
+    const HATEOAS = await prepareHATEOAS(joyas);
+    res.json(HATEOAS);
   } catch (err) {
     console.error(err);
     res.send(err.message);
